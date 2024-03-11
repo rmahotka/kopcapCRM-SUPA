ae9d0478dd716969f8fb5d1aa7a7acc517750e2f
mahotich <rmahotka@gmail.com>
2024-03-11 11:08:54 +0300

    format

--- src/stores/users.js
+++ src/stores/users.js
@@ -3,11 +3,11 @@ import { reactive } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
   const state = reactive({
-    user: null,
-  });
+    user: null
+  })
 
   const setUser = (payload) => {
-    state.user = payload ? payload.user : null;
+    state.user = payload ? payload.user : null
   }
 
   return { state, setUser }
